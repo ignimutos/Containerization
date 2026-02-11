6a1a0980a2e3bcc8b08d904e257218e74877f1d4
ignimutos <183905057+ignimutos@users.noreply.github.com>
2026-02-12 00:01:37 +0800

    fix: env setting problem

--- naive-server/misc/entrypoint.sh
+++ naive-server/misc/entrypoint.sh
@@ -1,4 +1,20 @@
 #!/bin/ash
+set -e
+
+check_var() {
+  if [ -z "$1" ]; then
+    echo "错误：环境变量 $2 未设置！"
+    exit 1
+  fi
+}
+
+# 校验必须的环境变量（根据实际需求调整）
+check_var "$DOMAIN" "DOMAIN"
+check_var "$EMAIL" "EMAIL"
+check_var "$USER" "USER"
+check_var "$PASS" "PASS"
+check_var "$REVERSE_SERVER" "REVERSE_SERVER"
+
 if [ -e /etc/caddy/Caddyfile ]; then
   cat <<EOF >/etc/caddy/Caddyfile
 {
